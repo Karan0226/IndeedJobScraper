f9d864e6c603cb0573215b9db841413a77d9c0a7
Karan Sahani <106232809+Karan0226@users.noreply.github.com>
2025-02-18 15:27:17 +0530

    Delete job_project directory

--- job_project/jobapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Job(models.Model):
-    title = models.CharField(max_length=255)
-    company = models.CharField(max_length=255)
-    location = models.CharField(max_length=255)
-    link = models.URLField()
